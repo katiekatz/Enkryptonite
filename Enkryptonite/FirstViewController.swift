bc352d9326d54011ca40ac819e5c44fcd285d85f
Katie <katkatz@bergen.org>
2015-11-07 16:42:59 -0500

    Initial Commit

---
+++ Enkryptonite/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Enkryptonite
+//
+//  Created by Katie Katz on 11/7/15.
+//  Copyright (c) 2015 Katie Katz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
